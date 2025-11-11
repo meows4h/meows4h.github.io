4c6070f7f530ff7f8412a58b695f3736af015775
vixicat <dustyisntsorusty@gmail.com>
2025-11-10 21:43:02 -0800

    decoupling + improvements

---
+++ dist/js/utilities/init.js
@@ -0,0 +1,56 @@
+import * as THREE from 'three';
+
+export function init( animate ) {
+
+    // internal clock
+    const clock = new THREE.Clock();
+    
+    // scene setup
+    const scene = new THREE.Scene();
+    scene.background = new THREE.Color( 0xffffff ); // white fog
+    scene.fog = new THREE.Fog( 0xffffff, 0, 900 );
+    
+    // camera settings
+    const camera = new THREE.PerspectiveCamera( 100, window.innerWidth / window.innerHeight, 0.1, 1000 );
+    camera.rotation.order = 'YXZ';
+    camera.position.z = 5;
+    
+    // main lighting
+    const mainLight = new THREE.HemisphereLight( 0x8dc1de, 0x00668d, 1.5 );
+    mainLight.position.set( 2, 1, 1 );
+    scene.add( mainLight );
+    
+    // directional lighting
+    const directionalLight = new THREE.DirectionalLight( 0xffffff, 2.5 );
+    directionalLight.position.set( - 5, 25, - 1 );
+    directionalLight.castShadow = true;
+    directionalLight.shadow.camera.near = 0.01;
+    directionalLight.shadow.camera.far = 500;
+    directionalLight.shadow.camera.right = 30;
+    directionalLight.shadow.camera.left = - 30;
+    directionalLight.shadow.camera.top	= 30;
+    directionalLight.shadow.camera.bottom = - 30;
+    directionalLight.shadow.mapSize.width = 1024;
+    directionalLight.shadow.mapSize.height = 1024;
+    directionalLight.shadow.radius = 4;
+    directionalLight.shadow.bias = - 0.00006;
+    scene.add( directionalLight );
+    
+    // set camera targets
+    transition(1);
+    
+    // renderer
+    const renderer = new THREE.WebGLRenderer( { antialias: true } );
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.setAnimationLoop( animate );
+    renderer.shadowMap.enabled = true;
+    renderer.shadowMap.type = THREE.VSMShadowMap;
+    renderer.toneMapping = THREE.ACESFilmicToneMapping;
+    
+    const mainContainer = document.getElementById( 'main-container' );
+    mainContainer.appendChild( renderer.domElement );
+
+    return [clock, scene, camera, mainLight, directionalLight, renderer, mainContainer]
+
+}
---
+++ dist/js/utilities/tween.js
@@ -0,0 +1,92 @@
+function degreesToRadians(degrees) {
+  return degrees * (Math.PI / 180);
+}
+
+function easeInOutSine(t) {
+    return -(Math.cos(Math.PI * t) - 1) / 2;
+}
+
+function easeInOutQuad(t) {
+    return t < 0.5 ? 2*t*t : 1 - Math.pow(-2*t + 2, 2) / 2;
+}
+
+function easeOutCubic(t) {
+    return 1 - Math.pow(1 - t, 3);
+}
+
+export function tween(pos, rot, time_elapsed, trans_time, trans_type) {
+
+    if (trans_time == 0) trans_time = 1;
+    let time_shift = time_elapsed / trans_time;
+    if (time_shift > 1) time_shift = 1;
+    if (time_shift < 0) time_shift = 0;
+
+    let shift;
+    if (trans_type == 1) {
+        shift = easeInOutSine(time_shift);
+    } else if (trans_type == 2) {
+        shift = easeInOutQuad(time_shift);
+    } else if (trans_type == 3) {
+        shift = easeOutCubic(time_shift);
+    } else {
+        shift = time_shift;
+    }
+
+    const x_val = pos.x + ((pos.tx - pos.x) * shift);
+    const y_val = pos.y + ((pos.ty - pos.y) * shift);
+    const z_val = pos.z + ((pos.tz - pos.z) * shift);
+
+    const x_rot = rot.x + ((rot.tx - rot.x) * shift);
+    const y_rot = rot.y + ((rot.ty - rot.y) * shift);
+    const z_rot = rot.z + ((rot.tz - rot.z) * shift);
+    
+    return [x_val, y_val, z_val, x_rot, y_rot, z_rot];
+
+}
+
+export function set_state(pos, rot, camera) {
+    pos.x = camera.position.x;
+    pos.y = camera.position.y;
+    pos.z = camera.position.z;
+
+    rot.x = camera.rotation.x;
+    rot.y = camera.rotation.y;
+    rot.z = camera.rotation.z;
+}
+
+export function transition(pos, rot, old, curr) {
+
+    if (old == curr) {
+        return;
+    }
+
+    if (page_state == 0) {
+
+        pos.tx = 0;
+        pos.ty = 0;
+        pos.tz = 5;
+        rot.tx = 0;
+        rot.ty = 0;
+        rot.tz = 0;
+
+    } else if (page_state == 1) {
+
+        pos.tx = 5;
+        pos.ty = 5;
+        pos.tz = 0;
+        rot.tx = degreesToRadians( -90 );
+        rot.ty = degreesToRadians( -20 );
+        rot.tz = degreesToRadians( -5 );
+
+    } else if (page_state == 2) {
+
+        pos.tx = 0;
+        pos.ty = 0;
+        pos.tz = 5;
+        rot.tx = degreesToRadians( -90 );
+        rot.ty = 0;
+        rot.tz = 0;
+
+    }
+
+}
