d2b90ed2242ecf143b8da690b76754ab32d98f3e
meows4h <meows4h@users.noreply.github.com>
2026-05-08 06:25:31 +0000

    Updates

--- js/top-prio.js
+++ js/top-prio.js
@@ -1,3 +1,13 @@
+function permute(arr) {
+    for (var i = arr.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    return arr;
+}
+
 function permuteParty() {
     var arr = [1, 2, 3, 4, 5, 6, 7, 8];
     return permute(arr);
